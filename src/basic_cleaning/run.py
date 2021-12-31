b23f98d145a2af2c792c54f64a1fb07f7a7fed7f
Wei Chiet <weichiet@outlook.com>
2022-01-01 01:05:05 +0800

    Complete basic_cleaning code

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -4,8 +4,9 @@ Download from W&B the raw dataset and apply some basic data cleaning, exporting 
 """
 import argparse
 import logging
+import os
 import wandb
-
+import pandas as pd
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
@@ -16,14 +17,40 @@ def go(args):
     run = wandb.init(job_type="basic_cleaning")
     run.config.update(args)
 
-    # Download input artifact. This will also log that this script is using this
-    # particular version of the artifact
-    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+    # Download input artifact.
+    logger.info("Downloading artifact")
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+    df = pd.read_csv(artifact_path)
 
-    ######################
-    # YOUR CODE HERE     #
-    ######################
+    # Drop outliers
+    logger.info("Dropping outliers")
+    min_price = args.min_price
+    max_price = args.max_price
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
 
+    # Convert last_review to datetime
+    logger.info("Converting last_review to datetime type")
+    df['last_review'] = pd.to_datetime(df['last_review'])
+
+    #Save new df to CSV without the index column
+    logger.info("Saving new CSV")
+    filename = "clean_sample.csv"
+    df.to_csv(filename, index=False)
+
+    #Upload the file to W&B
+    logger.info("Creating artifact")
+    artifact = wandb.Artifact(
+        name=args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file(filename)
+
+    logger.info("Logging artifact")
+    run.log_artifact(artifact)
+    os.remove(filename)
 
 if __name__ == "__main__":
 
@@ -31,45 +58,45 @@ if __name__ == "__main__":
 
 
     parser.add_argument(
-        "--input_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--input_artifact",
+        type = str,
+        help = "Fully-qualified name for the input artifact",
+        required = True
     )
 
     parser.add_argument(
-        "--output_artifact", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--output_artifact",
+        type = str,
+        help = "Name for the artifact",
+        required = True
     )
 
     parser.add_argument(
-        "--output_type", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--output_type",
+        type = str,
+        help = "Type for the artifact",
+        required = True
     )
 
     parser.add_argument(
-        "--output_description", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--output_description",
+        type = str,
+        help = "Description for the artifact",
+        required = True
     )
 
     parser.add_argument(
-        "--min_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--min_price",
+        type = float,
+        help = "Minimum price per night",
+        required = True
     )
 
     parser.add_argument(
-        "--max_price", 
-        type=## INSERT TYPE HERE: str, float or int,
-        help=## INSERT DESCRIPTION HERE,
-        required=True
+        "--max_price",
+        type = float,
+        help = "Maximun price per night",
+        required = True
     )
 
 
