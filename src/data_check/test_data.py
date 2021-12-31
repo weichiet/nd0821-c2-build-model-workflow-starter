fca97aa2fc83bf5f92822099b82c3299f8ac8f6f
Wei Chiet <weichiet@outlook.com>
2022-01-01 02:02:35 +0800

    Add test_row_count and test_price_range functions

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,3 +63,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 ########################################################
 # Implement here test_row_count and test_price_range   #
 ########################################################
+def test_row_count(data):
+    """
+    The the number of row of the dataset is within the boundaries
+    """
+    assert 15000 < data.shape[0] < 1000000
+
+def test_price_range(data, min_price, max_price):
+    """
+    Test the price range for one night
+    """
+
+    assert data['price'].dropna().between(min_price, max_price).all()
